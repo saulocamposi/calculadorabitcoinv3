070163b4d74f178ca649c0b7774c7801f2b348dd
Saulo Camposi <saulocamposi@gmail.com>
2017-06-27 00:59:03 +0100

    Add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FoxbitComponent } from './foxbit/foxbit.component';
+import { BrexchangeComponent } from './brexchange/brexchange.component';
+import { BrexchangeDirective } from './brexchange.directive';
+import { NavComponent } from './nav/nav.component';
+import { PolionexComponent } from './polionex/polionex.component';
+import { BitvalorComponent } from './bitvalor/bitvalor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrexchangeComponent,
+    NavComponent,
+    PolionexComponent,
+    BitvalorComponent,
   ],
   imports: [
     BrowserModule,
