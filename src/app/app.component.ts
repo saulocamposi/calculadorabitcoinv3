8c512d921776ec4f46a25dfff82097d2da1ab815
Saulo Camposi <saulocamposi@gmail.com>
2017-08-01 04:13:16 +0100

    Update fetch test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private http: Http){
+
+    let link = "https://api.github.com/";
+    let blink = "https://api.kraken.com/0/public/Ticker?pair=XBTEUR";
+    let block = "http://localhost/calculadorabitcoinv2/backend/ws/services/services.php?brand=fox";
+
+    this.http.request(blink).subscribe((res: Response) => console.log(res.json()));
+    this.http.request(link).subscribe((res: Response) => console.log(res.json()));
+    this.http.request(block).subscribe((res: Response) => console.log(res.json()));
+
+
+
+  }
+
+
+
+
+
+
 }
